6c850bb1e4e2debe0847cad4d1cbdb650abb5735
Juan Manuel Barreneche <jumanbar@gmail.com>
2023-09-20 13:31:51 -0300

    Source

--- lua/tocnavigator/justlearning.lua
+++ lua/tocnavigator/justlearning.lua
@@ -2,6 +2,10 @@
 local q = require("lua.tocnavigator.queries")
 -- local x = vim.cmd [[BufReadCmd lua/tocnavigator/tests/test.md]]
 
+--[[
+    Source:
+    https://github.com/nvim-neo-tree/neo-tree.nvim/blob/e968cda658089b56ee1eaa1772a2a0e50113b902/lua/neo-tree/utils.lua#L157-L165
+]]
 local find_buffer_by_name = function(name)
     for _, buf in ipairs(vim.api.nvim_list_bufs()) do
         local buf_name = vim.api.nvim_buf_get_name(buf)
